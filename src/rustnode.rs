0664ac6412d7e72baa4350dc3317806284555d9a
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-03-21 16:32:26 -0400

    dunno

--- src/rustnode.rs
+++ src/rustnode.rs
@@ -2,12 +2,10 @@ use godot::prelude::*;
 use godot::engine::Node2D;
 
 
+
 #[derive(GodotClass)]
 #[class(base=Node2D)]
 struct RustNode {
-    speed: f64,
-    angular_speed: f64,
-
     base: Base<Node2D>
 }
 
@@ -15,12 +13,9 @@ struct RustNode {
 #[godot_api]
 impl INode2D for RustNode {
     fn init(base: Base<Node2D>) -> Self {
-        godot_print!("Hello, World!");
 
         Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
-            base,
+            base, 
         }
     }
 }
